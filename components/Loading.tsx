b61bc94fb8e62191d78b88acf2e1d63047e035a1
HomieZ09 <phumsoen123@gmail.com>
2024-10-01 11:48:07 +0700

    refactor name of loading.gif:

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -6,7 +6,7 @@ export default function Loading({ visible, text='กำลังโหลด...'
     if (!visible) return null;
     return (
         <View style={{ position: 'absolute', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', zIndex: 999, backgroundColor: colors.black, opacity: 0.8  }}>
-            <Image source={require('@/assets/images/loading.gif')} style={{ width: 150, height: 150, marginBottom: 15 }} />
+            <Image source={require('@/assets/images/loading2.gif')} style={{ width: 150, height: 150, marginBottom: 15 }} />
             <SetText color={colors.white} type='bold'>{text}</SetText>
         </View>
     )
