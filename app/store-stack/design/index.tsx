de4fb4dec0ad8b5c2f1a6d616ac18798c927ed85
HomieZ09 <phumsoen123@gmail.com>
2024-10-09 00:01:59 +0700

    Add Edit on Design Page

--- app/store-stack/design/index.tsx
+++ app/store-stack/design/index.tsx
@@ -5,8 +5,8 @@ import { useNavigation, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { TouchableOpacity, View, Image, ScrollView, NativeSyntheticEvent, NativeScrollEvent, Alert } from "react-native";
 import { Iconify } from "react-native-iconify";
-import RNPickerSelect from 'react-native-picker-select';
 import { Dropdown } from 'react-native-element-dropdown';
+import { ImageLibraryOptions, launchImageLibrary } from "react-native-image-picker";
 
 type IDesign = {
     design_id: number,
@@ -180,12 +180,15 @@ const TagItem = ({ item, isSelected, setSelectedTag }: { item: IDesignTag, isSel
     );
 }
 
-const CardItem = ({ item }: { item: IDesign }) => {
+const CardItem = ({ item, setDesignId, setPopup }: { item: IDesign, setDesignId: (value: number) => void, setPopup: (value: boolean) => void }) => {
     const router = useRouter();
 
     return (
         <View style={{ width: '45%', height: 260, marginBottom: 10 }}>
-            <TouchableOpacity onPress={() => router.push(`/user-stack/add-information/${item.design_id}`)} style={{ width: '100%', height: 200, borderRadius: 10 }}>
+            <TouchableOpacity onPress={() => {
+                setDesignId(item.design_id);
+                setPopup(true);
+                }} style={{ width: '100%', height: 200, borderRadius: 10 }}>
                 <Image source={require('@/assets/images/promote.png')} style={{ width: '100%', height: '100%', borderRadius: 10 }} />
             </TouchableOpacity>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
@@ -204,7 +207,9 @@ export default function Design() {
     const navigation = useNavigation();
     const [scrollY, setScrollY] = useState<number>(0);
     const [selectedTag, setSelectedTag] = useState<number>(0);
-    const [isPopup, setIsPopup] = useState<boolean>(false);
+    const [isPopupAdd, setIsPopupAdd] = useState<boolean>(false);
+    const [isPopupEdit, setIsPopupEdit] = useState<boolean>(false);
+    const [designId, setDesignId] = useState<number>(0);
 
     useEffect(() => {
         let title = ''
@@ -254,27 +259,42 @@ export default function Design() {
                 <View style={{ marginTop: '5%', marginHorizontal: '5%', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
                     {filteredData().map((item: any, index: number) => {
                         return (
-                            <CardItem key={index} item={item} />
+                            <CardItem key={index} item={item} setDesignId={setDesignId} setPopup={setIsPopupEdit}  />
                         )
                     })}
                 </View>
             </ScrollView>
-            <TouchableOpacity onPress={() => setIsPopup(true)} style={{ position: 'absolute', width: 70, height: 70, bottom: 0, right: 0, margin: 15, borderRadius: 999, backgroundColor: colors.white, alignItems: 'center', justifyContent: 'center', ...styles.shadowCustom }}>
+            <TouchableOpacity onPress={() => setIsPopupAdd(true)} style={{ position: 'absolute', width: 70, height: 70, bottom: 0, right: 0, margin: 15, borderRadius: 999, backgroundColor: colors.white, alignItems: 'center', justifyContent: 'center', ...styles.shadowCustom }}>
                 <Iconify icon='fluent-emoji-high-contrast:plus' size={40} color={colors.mediumpink} />
             </TouchableOpacity>
-            {isPopup && <Popup action="add" setIsShow={setIsPopup} />}
+            {isPopupAdd && <Popup action="add" setIsShow={setIsPopupAdd} />}
+            {isPopupEdit && <Popup action="edit" design_id={designId} setIsShow={setIsPopupEdit} />}
         </WrapBackground>
     );
 }
 
-const Popup = ({ action, design_id, setIsShow }: { action: 'add' | 'edit', design_id?: string, setIsShow: (value: boolean) => void }) => {
-    const [name, setName] = useState<string>('');
+const Popup = ({ action, design_id, setIsShow }: { action: 'add' | 'edit', design_id?: number, setIsShow: (value: boolean) => void }) => {
     const [value, setValue] = useState<any>(null);
     const [isFocus, setIsFocus] = useState(false);
+    const [photo, setPhoto] = useState<any>(null);
+
+    const handleChoosePhoto = () => {
+        const options: ImageLibraryOptions = {
+            mediaType: 'photo',
+            quality: 1,
+        };
+        launchImageLibrary(options, (response) => {
+            console.log('Response = ', response);
+
+            if (!response.didCancel) {
+                setPhoto(response);
+            }
+        });
+    }
 
     return (
         <View style={{ position: 'absolute', height: '100%', width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-            <View style={{ backgroundColor: colors.backgroundColor, width: '100%', height: '60%', position: 'absolute', bottom: 0, alignSelf: 'center', borderRadius: 16, padding: 20 }}>
+            <View style={{ backgroundColor: colors.backgroundColor, width: '100%', height: '65%', position: 'absolute', bottom: 0, alignSelf: 'center', borderRadius: 16, padding: 20 }}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                     <SetText type="bold" size={20}>{action === 'add' ? 'เพิ่มแบบ' : 'แก้ไขแบบ'}</SetText>
                     <TouchableOpacity onPress={() => setIsShow(false)}><Iconify icon="bx:bx-x" size={24} color={colors.grey} /></TouchableOpacity>
@@ -307,8 +327,14 @@ const Popup = ({ action, design_id, setIsShow }: { action: 'add' | 'edit', desig
                 </View>
                 <View style={{ marginTop: 20, borderColor: colors.line, paddingBottom: 20, gap: 10 }}>
                     <SetText style={{ marginTop: 8 }} type="bold">อัปโหลดแบบที่ต้องการ</SetText>
-                    <TouchableOpacity style={{ width: 160, height: 224, borderRadius: 8, ...styles.shadowCustom, backgroundColor: colors.white, alignItems: 'center', justifyContent: 'center' }}>
+                    {/* <TouchableOpacity style={{ width: 160, height: 224, borderRadius: 8, ...styles.shadowCustom, backgroundColor: colors.white, alignItems: 'center', justifyContent: 'center' }}>
                         <Iconify icon="bx:bx-image-add" size={40} color={colors.grey} />
+                    </TouchableOpacity> */}
+                    <TouchableOpacity onPress={handleChoosePhoto} style={{ width: 160, height: 224, marginTop: 10, borderRadius: 10, backgroundColor: colors.white, ...styles.shadowCustom, alignItems: 'center', justifyContent: 'center' }}>
+                        {photo && <TouchableOpacity onPress={() => setPhoto(null)} style={{ position: 'absolute', top: -10, right: -10, zIndex: 99 }}><Iconify icon="mdi:cross-circle" size={24} color={colors.black} /></TouchableOpacity>}
+                        {photo ? <Image source={{ uri: photo.assets[0].uri }} style={{ width: '100%', height: '100%', borderRadius: 10 }} />
+                            : <Iconify icon="fluent-emoji-high-contrast:plus" size={40} color={colors.line} />
+                        }
                     </TouchableOpacity>
                 </View>
                 {/* <View style={{ flexDirection: 'column', gap: 5 }}>
@@ -339,8 +365,8 @@ const Popup = ({ action, design_id, setIsShow }: { action: 'add' | 'edit', desig
                     />
                 </View> */}
             </View>
-            <TouchableOpacity disabled={!(name.length > 0)} style={{ borderTopWidth: 0.5, marginTop: 10, position: 'absolute', bottom: 0, height: 80, width: '100%', borderTopStartRadius: 8, borderTopEndRadius: 8, justifyContent: 'center', paddingHorizontal: 20, borderColor: colors.line }}>
-                <View style={{ padding: 10, alignItems: 'center', borderRadius: 999, backgroundColor: name.length > 0 ? colors.mediumpink : colors.lesspink }}>
+            <TouchableOpacity disabled={!(value > 0)} style={{ borderTopWidth: 0.5, marginTop: 10, position: 'absolute', bottom: 0, height: 80, width: '100%', borderTopStartRadius: 8, borderTopEndRadius: 8, justifyContent: 'center', paddingHorizontal: 20, borderColor: colors.line }}>
+                <View style={{ padding: 10, alignItems: 'center', borderRadius: 999, backgroundColor: value > 0 ? colors.mediumpink : colors.lesspink }}>
                     <SetText type='bold' color={colors.white}>บันทึก</SetText>
                 </View>
             </TouchableOpacity>
