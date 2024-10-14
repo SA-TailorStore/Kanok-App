9d7b4c1b3914aea8069f1e6f18c4529335d16ac7
HomieZ09 <phumsoen123@gmail.com>
2024-10-14 23:58:24 +0700

    resize button

--- components/order-button/AssignWorkButton.tsx
+++ components/order-button/AssignWorkButton.tsx
@@ -1,14 +1,12 @@
 import { SetText } from "@/components/SetText";
 import { colors } from "@/utils/styles";
 import { useRouter } from "expo-router";
-import { Linking, TouchableOpacity, View } from "react-native";
-import { Iconify } from "react-native-iconify";
-
+import { TouchableOpacity } from "react-native";
 
 export function AssignWorkButton({ order_id }: { order_id: string }) {
     const router = useRouter();
     return (
-        <TouchableOpacity onPress={() => router.push(`/store-stack/assign-work/${order_id}`)} style={{ flexDirection: 'row', gap: 2, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.nopink, opacity: 0.7,borderRadius: 8, padding: 10, borderColor: colors.mediumpink, borderWidth: 1 }}>
+        <TouchableOpacity onPress={() => router.push(`/store-stack/assign-work/${order_id}`)} style={{ flexDirection: 'row', gap: 2, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.nopink, opacity: 0.7,borderRadius: 8, padding: 8, borderColor: colors.mediumpink, borderWidth: 1 }}>
             <SetText type="bold" size={12} color={colors.mediumpink}>มอบหมายงานให้ช่าง</SetText>
         </TouchableOpacity>
     );
