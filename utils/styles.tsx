b6f6d67d8353732b29bc20332cc52d149cb71533
HomieZ09 <phumsoen123@gmail.com>
2024-10-15 16:41:57 +0700

    remove comment

--- utils/styles.tsx
+++ utils/styles.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, StatusBar, StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
@@ -25,7 +25,6 @@ export const headerTitleStyle = {
     fontSize: 25,
 };
 
-// tip: ไม่ต้องเขียน css ในนี้เขียนที่ไฟล์ของหน้านั้นๆเลย ง่ายกว่า!!!! 
 export const styles = StyleSheet.create({
     logoUserContainer: {
         display: 'flex',
